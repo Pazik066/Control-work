af73e4ca646c9ae4f29c540c0851b20d929c5f88
pazik <pazik066@mail.ru>
2023-12-01 16:10:52 +0300

    add method PrintArray

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] array1 = new string[5] {"Hello", "world", "2", "23", "!"};
 string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
+void SecondArrayWithIf(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
@@ -12,3 +12,13 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIf(array1, array2);
+PrintArray(array2);
